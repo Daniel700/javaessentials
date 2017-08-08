2e858394b8a84483b935da810212c7b46a8991b0
Daniel Bäuml <d.baeuml@hotmail.de>
2017-08-08 13:32:50 +0200

    singleton pattern

---
+++ src/main/java/pattern_singleton/SingletonTwo.java
@@ -0,0 +1,19 @@
+package pattern_singleton;
+
+/**
+ * This approach creates the singleton eagerly instead of lazily.
+ * (When the class is loaded from the JVM the singleton is created)
+ */
+public class SingletonTwo {
+
+    private static SingletonTwo instance = new SingletonTwo();
+
+    private SingletonTwo(){
+
+    }
+
+    public static SingletonTwo getInstance(){
+        return instance;
+    }
+
+}
